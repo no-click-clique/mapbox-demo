172f0c4a01d31b179ad13b0bf346aaf3a653159f
dani <dani.zaghian@gmail.com>
2020-03-02 12:28:50 -0800

    adds mapbox library

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+  #map { top: 0; bottom: 0; width: 100%; height: 700px; }
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  mounted: function() {
+    mapboxgl.accessToken = '<your access token>';
+    var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-87.623177, 41.881832], // starting position [lng, lat]
+        zoom: 12 // starting zoom
+    });
+  },
+  methods: {}
+};
 </script>
